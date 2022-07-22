17399cb6a324507dc50a744ec70bcbc86a87ce45
Diana <diana45637@live.com>
2022-07-22 00:01:27 -0300

    feature/validation-cadastro

---
+++ pages/scripts/cadastro.js
@@ -0,0 +1,69 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.getElementById('signup');
+
+const required = "Campo Obrigatório";
+const EMAIL_INVALID = "Formato de e-mail incorreto";
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let emailValid = hasValue(form.elements['email'], required, EMAIL_INVALID);
+  let nameValid = hasValue(form.elements['nome'], required);
+  let passwordValid = hasValue(form.elements['password'], required);
+  let passwordConfirmValid = hasValue(form.elements['password-confirm'], required);
+
+  if (nameValid && emailValid && passwordValid && passwordConfirmValid) {
+    form.submit();
+  }
+})
